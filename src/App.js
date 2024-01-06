7dc001a3dc0d0342b44b5d6fa585e85cd1f6c845
KRSNaveen <krsnaveen876@gmail.com>
2024-01-07 04:46:25 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Products from './Components/Products';
+import { useState,useEffect } from 'react';
+import Example from './Components/Example';
+
 
 function App() {
+const[ items,setItems] =  useState([]);
+const [list, setlist]= useState([]);
+
+const collect = async ()=>{
+  let response = await fetch("https://tshirtsdata-ae673-default-rtdb.firebaseio.com/data.json");
+  let data = await response.json();
+  console.log(data);
+  if(data != null)
+  {
+    setItems(Object.values(data));
+    setlist(Object.values(data));
+  }
+}
+
+
+useEffect( ()=>{
+    collect();
+     
+   },[]);
+
+
+
+
+function collectdata(data){
+     setItems([...items, data]);
+  }
+  console.log(items);
+       
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <header>
+          <Example listitems={list}/>
+          <Header collectdata={collectdata}/>
+          <Products items={items} col={setlist}/>
+     </header>
   );
 }
 
